d2c2d76bf335ba66c8d466c56934f6015512a91c
Firdaus Kamaruddin <mail4frenz@gmail.com>
2020-04-26 14:10:08 +0800

    add Instructor controller

---
+++ src/TuitionApp.Api/Controllers/InstructorController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TuitionApp.Core.Features.Common;
+using TuitionApp.Core.Features.Instructor;
+
+namespace TuitionApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructorController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public InstructorController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetInstructorList")]
+        public async Task<ActionResult<GetObjectListVm<GetInstructorItemDto>>> GetInstructorList()
+        {
+            var vm = await mediator.Send(new GetInstructorListQuery());
+            return Ok(vm);
+        }
+
+        [HttpGet("{InstructorId}", Name = "GetInstructorItem")]
+        public async Task<ActionResult<GetInstructorItemDto>> GetInstructorItem(Guid InstructorId)
+        {
+            var vm = await mediator.Send(new GetInstructorItemQuery() { Id = InstructorId });
+
+            return Ok(vm);
+        }
+
+        [HttpPost(Name = "CreateInstructorItem")]
+        public async Task<ActionResult<Guid>> CreateInstructorItem(CreateInstructorItemCommand itemCommand)
+        {
+            var vm = await mediator.Send(itemCommand);
+            if (vm.Id != null)
+            {
+                var link = Url.Link("GetInstructorItem", new { InstructorId = vm.Id });
+                return Created(link, vm);
+            }
+            else
+            {
+                return BadRequest(vm);
+            }
+
+        }
+    }
+}
