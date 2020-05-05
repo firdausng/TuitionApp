e49f2307fc96af58f9181bbf5c5c7f4d9a91b240
Firdaus Kamaruddin <mail4frenz@gmail.com>
2020-05-05 13:28:52 +0800

    add navigation element

---
+++ src/TuitionApp.Spa/ClientApp/src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+const componentList = [MainNavComponent]
+
+@NgModule({
+  declarations: componentList,
+  exports: componentList,
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class CoreModule { }
